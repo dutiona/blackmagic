6b6870a3e86d11186c0bd3a24f00d5fbc8010a4c
Michaël Roynard <michaelroynard@gmail.com>
2018-01-08 15:32:57 +0100

    Cleanup

--- example/metaprog/cpt/pixel.cpp
+++ example/metaprog/cpt/pixel.cpp
@@ -7,27 +7,27 @@ namespace pixel_concept_traits {
 
 template <typename PixelType>
 using value_type_construct = typename PixelType::value_type;
-[[maybe_unused]] constexpr auto value_type_concept =
+constexpr auto value_type_concept =
   cpt::make_custom_concept_item_from_construct<value_type_construct>("value_type_concept"sv);
 
 template <typename PixelType>
 using reference_construct = typename PixelType::reference;
-[[maybe_unused]] constexpr auto reference_concept =
+constexpr auto reference_concept =
   cpt::make_custom_concept_item_from_construct<reference_construct>("reference_concept"sv);
 
 template <typename PixelType>
 using point_type_construct = typename PixelType::point_type;
-[[maybe_unused]] constexpr auto point_type_concept =
+constexpr auto point_type_concept =
   cpt::make_custom_concept_item_from_construct<point_type_construct>("point_type_concept"sv);
 
 template <typename PixelType>
 using site_type_construct = typename PixelType::site_type;
-[[maybe_unused]] constexpr auto site_type_concept =
+constexpr auto site_type_concept =
   cpt::make_custom_concept_item_from_construct<site_type_construct>("site_type_concept"sv);
 
 template <typename PixelType>
 using image_type_construct = typename PixelType::image_type;
-[[maybe_unused]] constexpr auto image_type_concept =
+constexpr auto image_type_concept =
   cpt::make_custom_concept_item_from_construct<image_type_construct>("image_type_concept"sv);
 
 // point_type alias site_type
@@ -36,7 +36,7 @@ using point_type_is_site_type_predicate = cpt::make_predicate<
   cpt::make_condition<cpt::check<PixelType>(point_type_concept)>,
   cpt::make_condition<cpt::check<PixelType>(site_type_concept)>,
   cpt::make_condition<std::is_same_v<site_type_construct<PixelType>, point_type_construct<PixelType>>>>;
-[[maybe_unused]] constexpr auto point_type_is_site_type_predicate_concept =
+constexpr auto point_type_is_site_type_predicate_concept =
   cpt::make_custom_concept_item_from_predicate<point_type_is_site_type_predicate>(
     "point_type_is_site_type_predicate_concept"sv);
 
@@ -45,7 +45,7 @@ template <typename PixelType>
 using value_type_is_not_a_reference_predicate =
   cpt::make_predicate<cpt::make_condition<cpt::check<PixelType>(value_type_concept)>,
                       cpt::make_condition<!std::is_reference_v<value_type_construct<PixelType>>>>;
-[[maybe_unused]] constexpr auto value_type_is_not_a_reference_predicate_concept =
+constexpr auto value_type_is_not_a_reference_predicate_concept =
   cpt::make_custom_concept_item_from_predicate<value_type_is_not_a_reference_predicate>(
     "value_type_is_not_a_reference_predicate_concept"sv);
 
@@ -55,7 +55,7 @@ using has_method_val_predicate =
   cpt::make_predicate<cpt::make_condition<cpt::check<PixelType>(reference_concept)>,
                       cpt::make_condition<cpt::check<cpt::concepts::invocable_r, reference_construct<PixelType>,
                                                      decltype(&PixelType::val), PixelType>()>>;
-[[maybe_unused]] constexpr auto has_method_val_predicate_concept =
+constexpr auto has_method_val_predicate_concept =
   cpt::make_custom_concept_item_from_predicate<has_method_val_predicate>("has_method_val_predicate_concept"sv);
 
 // has method point
@@ -64,7 +64,7 @@ using has_method_point_predicate =
   cpt::make_predicate<cpt::make_condition<cpt::check<PixelType>(point_type_concept)>,
                       cpt::make_condition<cpt::check<cpt::concepts::invocable_r, point_type_construct<PixelType>,
                                                      decltype(&PixelType::point), PixelType>()>>;
-[[maybe_unused]] constexpr auto has_method_point_predicate_concept =
+constexpr auto has_method_point_predicate_concept =
   cpt::make_custom_concept_item_from_predicate<has_method_point_predicate>("has_method_point_predicate_concept"sv);
 
 // has method site
@@ -73,7 +73,7 @@ using has_method_site_predicate =
   cpt::make_predicate<cpt::make_condition<cpt::check<PixelType>(site_type_concept)>,
                       cpt::make_condition<cpt::check<cpt::concepts::invocable_r, site_type_construct<PixelType>,
                                                      decltype(&PixelType::site), PixelType>()>>;
-[[maybe_unused]] constexpr auto has_method_site_predicate_concept =
+constexpr auto has_method_site_predicate_concept =
   cpt::make_custom_concept_item_from_predicate<has_method_site_predicate>("has_method_site_predicate_concept"sv);
 
 // has method image
@@ -82,13 +82,13 @@ using has_method_image_predicate =
   cpt::make_predicate<cpt::make_condition<cpt::check<PixelType>(image_type_concept)>,
                       cpt::make_condition<cpt::check<cpt::concepts::invocable_r, image_type_construct<PixelType>,
                                                      decltype(&PixelType::image), PixelType>()>>;
-[[maybe_unused]] constexpr auto has_method_image_predicate_concept =
+constexpr auto has_method_image_predicate_concept =
   cpt::make_custom_concept_item_from_predicate<has_method_image_predicate>("has_method_image_predicate_concept"sv);
 
 
 } // namespace pixel_concept_traits
 
-[[maybe_unused]] constexpr auto pixel_concept = cpt::make_concept_map(
+constexpr auto pixel_concept = cpt::make_concept_map(
   pixel_concept_traits::value_type_concept, pixel_concept_traits::reference_concept,
   pixel_concept_traits::point_type_concept, pixel_concept_traits::site_type_concept,
   pixel_concept_traits::image_type_concept, pixel_concept_traits::point_type_is_site_type_predicate_concept,
