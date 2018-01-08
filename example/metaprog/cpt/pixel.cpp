3c439f6aafbdaa4c2395438932586094c63908d1
Michaël Roynard <michaelroynard@gmail.com>
2018-01-08 10:50:25 +0100

    Make example on how to use those tools

--- example/metaprog/cpt/pixel.cpp
+++ example/metaprog/cpt/pixel.cpp
@@ -7,16 +7,23 @@ namespace pixel_concept_traits {
 
 template <typename PixelType>
 using has_value_type = typename PixelType::value_type;
-
 constexpr auto value_type_concept_item =
-  cpt::make_concept_item<cpt::custom_concept<has_value_type>>("value_type_concept"sv);
+  cpt::make_concept_item<cpt::custom_concept_construct<has_value_type>>("value_type_concept"sv);
+
+template <typename PixelType>
+using has_getV = cpt::traits::is_invocable_r<typename PixelType::value_type, decltype(&PixelType::getV), PixelType>;
+constexpr auto getV_concept_item =
+  cpt::make_concept_item<cpt::custom_concept_predicate<has_getV>>("has_getV_concept"sv);
+
 
 } // namespace pixel_concept_traits
 
-constexpr auto pixel_concept = cpt::make_concept_map(pixel_concept_traits::value_type_concept_item);
+constexpr auto pixel_concept =
+  cpt::make_concept_map(pixel_concept_traits::value_type_concept_item, pixel_concept_traits::getV_concept_item);
 
 struct MyPixel {
   using value_type = void;
+  value_type getV();
 };
 
 struct MyBadPixel {
