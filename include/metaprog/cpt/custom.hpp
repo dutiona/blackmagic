9b68fa575d8fd0d0aa19a51e33381d81fb5ac283
Michaël Roynard <michaelroynard@gmail.com>
2018-01-08 10:49:47 +0100

    Add a way to make a custom concept via a predicate

--- include/metaprog/cpt/custom.hpp
+++ include/metaprog/cpt/custom.hpp
@@ -12,22 +12,45 @@ template <typename...>
 struct _parameters_pack {
 };
 
-template <bool SilentFailure, template <typename...> class Contraint, typename Holder, typename = void>
-struct custom_concept_impl : std::false_type {
+template <bool SilentFailure, template <typename...> class Constraint, typename Holder, typename = void>
+struct custom_concept_construct_impl : std::false_type {
   static_assert(SilentFailure, "Custom concept failed");
 };
 
 template <bool SilentFailure, template <typename...> class Constraint, template <typename...> class ParametersPack,
           typename... Parameters>
-struct custom_concept_impl<SilentFailure, Constraint, ParametersPack<Parameters...>,
-                           std::void_t<Constraint<Parameters...>>> : std::true_type {
+struct custom_concept_construct_impl<SilentFailure, Constraint, ParametersPack<Parameters...>,
+                                     std::void_t<Constraint<Parameters...>>> : std::true_type {
+};
+
+template <bool SilentFailure, template <typename...> class Predicate, typename Holder, typename = void>
+struct custom_concept_predicate_impl : std::false_type {
+  static_assert(SilentFailure, "Custom concept failed");
+};
+
+template <bool SilentFailure, template <typename...> class Predicate, template <typename...> class ParametersPack,
+          typename... Parameters>
+struct custom_concept_predicate_impl<SilentFailure, Predicate, ParametersPack<Parameters...>,
+                                     std::enable_if_t<Predicate<Parameters...>::value>> : std::true_type {
 };
 } // namespace details
 
 template <template <typename...> class Constraint>
-struct custom_concept {
+struct custom_concept_construct {
   template <bool SilentFailure, typename... Parameters>
-  using type = details::custom_concept_impl<SilentFailure, Constraint, details::_parameters_pack<Parameters...>>;
+  using type =
+    details::custom_concept_construct_impl<SilentFailure, Constraint, details::_parameters_pack<Parameters...>>;
+  template <bool SilentFailure, typename... Parameters>
+  using underlying_type = typename type<SilentFailure, Parameters...>::type;
+  template <bool SilentFailure, typename... Parameters>
+  static constexpr bool value = type<SilentFailure, Parameters...>::value;
+};
+
+template <template <typename...> class Constraint>
+struct custom_concept_predicate {
+  template <bool SilentFailure, typename... Parameters>
+  using type =
+    details::custom_concept_predicate_impl<SilentFailure, Constraint, details::_parameters_pack<Parameters...>>;
   template <bool SilentFailure, typename... Parameters>
   using underlying_type = typename type<SilentFailure, Parameters...>::type;
   template <bool SilentFailure, typename... Parameters>
