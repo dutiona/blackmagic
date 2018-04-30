9c8b7f1cb92637c4a3de81b5a8955f1225e42925
Michaël Roynard <michaelroynard@gmail.com>
2018-04-30 22:20:23 +0200

    Fix concept_map so that it flattens and delete redundant concepts.

--- include/metaprog/concepts/engine/map.hpp
+++ include/metaprog/concepts/engine/map.hpp
@@ -13,8 +13,8 @@ namespace tuple = metaprog::tuple;
 
 namespace details {
 
-template <typename... Concepts>
-constexpr bool ensure_unique_keys(item<Concepts>... concept_items)
+template <typename... Items>
+constexpr bool ensure_unique_keys(Items&&... concept_items)
 {
   const auto items = std::make_tuple(concept_items...);
   return tuple::accumulate(
@@ -22,11 +22,11 @@ constexpr bool ensure_unique_keys(item<Concepts>... concept_items)
            [items](int count, auto cpt) {
              return count + tuple::count_if(items, [cpt_name = cpt.first](auto cpt_) { return cpt_.is(cpt_name); });
            })
-         == sizeof...(Concepts);
+         == sizeof...(Items);
 }
 
-template <typename... Concepts, size_t... I>
-constexpr bool ensure_unique_keys(std::tuple<item<Concepts>...> tuple_items, std::index_sequence<I...>)
+template <typename... Items, size_t... I>
+constexpr bool ensure_unique_keys(std::tuple<Items...> tuple_items, std::index_sequence<I...>)
 {
   return ensure_unique_keys(std::get<I>(tuple_items)...);
 }
@@ -34,30 +34,30 @@ constexpr bool ensure_unique_keys(std::tuple<item<Concepts>...> tuple_items, std
 } // namespace details
 
 
-template <typename... Concepts>
+template <typename... Items>
 struct map {
-  std::tuple<item<Concepts>...> items_;
+  const std::tuple<Items...> items_;
 
-  constexpr map(const item<Concepts>&... items)
-    : items_{items...}
+  constexpr map(Items&&... items)
+    : items_{std::forward<Items>(items)...}
   {
-    // if (!details::ensure_unique_keys(items...)) {
-    //  throw "Duplicate keys (concept name) in the concept map : the same concept is here twice ?";
-    //}
+    if (!details::ensure_unique_keys(items...)) {
+      throw "Duplicate keys (concept name) in the concept map : the same concept is here twice ?";
+    }
   }
 
-  constexpr map(const std::tuple<item<Concepts>...>& items)
+  constexpr map(const std::tuple<Items...>& items)
     : items_{items}
   {
-    // if (!details::ensure_unique_keys(items, std::index_sequence_for<item<Concepts>...>{})) {
-    //  throw "Duplicate keys (concept name) in the concept map : the same concept is here twice ?";
-    //}
+    if (!details::ensure_unique_keys(items, std::index_sequence_for<Items...>{})) {
+      throw "Duplicate keys (concept name) in the concept map : the same concept is here twice ?";
+    }
   }
 
 
   constexpr size_t count() const
   {
-    return std::tuple_size_v<map<Concepts...>>;
+    return std::tuple_size_v<decltype(items_)>;
   }
 
   constexpr size_t count_if(std::string_view concept_name) const
@@ -114,27 +114,25 @@ struct map {
 
 
 namespace details {
-/*
-  template <typename... Concepts>
-  constexpr auto make_map(const std::tuple<item<Concepts>...>& items) {
-    return map<Concepts...>{items};
-  }
 
-  template <typename... Concepts>
-  constexpr auto make_unique_map(const std::tuple<item<Concepts>...>& items) {
-    return make_map(tuple::unique(items));
-  }
-};
- */
+template <typename... Items>
+constexpr auto make_map(const std::tuple<Items...>& items)
+{
+  return map<Items...>{items};
+}
+
+template <typename... Items>
+constexpr auto make_unique_map(const std::tuple<Items...>& items)
+{
+  return make_map(tuple::unique(items));
+}
 
 // Merge maps functions
-template <typename... ConceptLhs, typename... ConceptRhs>
-constexpr auto merge_maps(const map<ConceptLhs...>& map_lhs, const map<ConceptRhs...>& map_rhs)
+template <typename... ItemsLhs, typename... ItemsRhs>
+constexpr auto merge_maps(const map<ItemsLhs...>& map_lhs, const map<ItemsRhs...>& map_rhs)
 {
-  // return details::make_unique_map(std::tuple_cat(map_lhs.items_, map_rhs.items_));
-  return map<ConceptLhs..., ConceptRhs...>{
-    std::tuple_cat(static_cast<std::tuple<item<ConceptLhs>...>>(map_lhs.items_),
-                   static_cast<std::tuple<item<ConceptRhs>...>>(map_rhs.items_))};
+  return details::make_unique_map(std::tuple_cat(map_lhs.items_, map_rhs.items_));
+  // return map<item<ConceptLhs>..., item<ConceptRhs>...>{std::tuple_cat(map_lhs.items_, map_rhs.items_)};
 }
 
 template <typename ConceptMap>
@@ -149,16 +147,16 @@ constexpr auto merge_all_maps(ConceptMap&& map, ConceptMaps&&... maps)
   return merge_maps(std::forward<ConceptMap>(map), merge_all_maps(std::forward<ConceptMaps>(maps)...));
 }
 
-template <typename... Concepts>
-constexpr decltype(auto) make_concept_map(map<Concepts...> map)
+template <typename... Items>
+constexpr decltype(auto) make_concept_map(map<Items...> map)
 {
   return map;
 }
 
 template <typename Concept>
-constexpr decltype(auto) make_concept_map(item<Concept> item)
+constexpr decltype(auto) make_concept_map(item<Concept> it)
 {
-  return map<Concept>{item};
+  return map<item<Concept>>{it};
 }
 } // namespace details
 
