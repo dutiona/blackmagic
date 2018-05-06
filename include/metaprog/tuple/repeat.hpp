ed8d70336132bcd2526d26126fb4a333312f5dbd
Michaël Roynard <michaelroynard@gmail.com>
2018-05-06 21:24:50 +0200

    Add repeat algorithm

---
+++ include/metaprog/tuple/repeat.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "../type/type.hpp"
+
+#include <tuple>
+#include <utility>
+
+namespace metaprog::tuple { inline namespace algorithm {
+
+namespace type = metaprog::type;
+
+struct repeat_t {
+  template <typename T, size_t N>
+  constexpr auto operator()(T&& e, type::size_t_<N>) const;
+};
+
+inline constexpr repeat_t repeat{};
+
+template <typename T, size_t N>
+constexpr auto repeat_t::operator()(T&& e, type::size_t_<N>) const
+{
+  if constexpr (N == 0) {
+    return std::make_tuple();
+  }
+  else {
+    return push_front(repeat(std::forward<T>(e), type::size_t_<N-1>{}), std::forward<T>(e));
+  }
+}
+
+}} // namespace metaprog::tuple::algorithm
