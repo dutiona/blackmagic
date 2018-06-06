ffe158b3f4dcaf2b69a665ae61a384dae04a8249
Michaël Roynard <michael.roynard@lrde.epita.fr>
2018-06-06 15:01:40 +0200

    Add conan support

---
+++ conanfile.py
@@ -0,0 +1,30 @@
+from conans import CMake, ConanFile, tools
+
+class GtestconstexprextConan(ConanFile):
+    name = "Blackmagic"
+    version = "0.4"
+    license = "MIT"
+    url = "https://gitlab.lrde.epita.fr/mroynard/blackmagic"
+    description = "Metaprogramming toolbox to do blackmagic in C++17."
+
+    settings = "os", "compiler", "arch", "build_type", "cppstd"
+    requires = "gtest/1.8.0@bincrafters/stable"#, "benchmark/1.8.0@lrde-local/stable"
+    generators = "cmake"
+
+    no_copy_source = True
+
+    def source(self):
+        git = tools.Git()
+        git.clone("https://gitlab.lrde.epita.fr/mroynard/blackmagic.git")
+
+    def build(self):
+        cmake = CMake(self, generator="Ninja")
+        cmake.configure()
+        cmake.build()
+        cmake.test()
+
+    def package(self):
+        self.copy("*.hpp", dst="include/blackmagic", src="include/blackmagic")
+
+    def package_id(self):
+        self.info.header_only()
---
+++ test_package/test.cpp
@@ -0,0 +1,5 @@
+#include <blackmagic/blackmagic.hpp>
+
+int main() {
+  return 0;
+}
