6d7ab72d0838e511c8002c5933907750dbd47654
Michaël Roynard <michaelroynard@gmail.com>
2018-07-04 21:48:39 +0200

    Consistency on name

--- include/blackmagic/type/list.hpp
+++ include/blackmagic/type/list.hpp
@@ -11,7 +11,7 @@ namespace blackmagic::type {
 using npos = integral::long_long_t<-1>;
 
 template <typename... Ts>
-struct basic_list {
+struct basic_type_list {
   static constexpr size_t size() noexcept
   {
     return sizeof...(Ts);
