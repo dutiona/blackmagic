d6ba22b1a89bcd2dda035d1053214fb8de801ac0
Michaël Roynard <michael.roynard@lrde.epita.fr>
2018-10-11 17:52:10 +0200

    Upgrade conan and CI integration Update project options Update CMakeLists.txt

--- conanfile.py
+++ conanfile.py
@@ -1,26 +1,34 @@
 from conans import CMake, ConanFile, tools
 
+
 class Blackmagic(ConanFile):
     name = "blackmagic"
     version = "0.4"
     license = "MIT"
     url = "https://gitlab.lrde.epita.fr/mroynard/blackmagic"
     description = "Metaprogramming toolbox to do blackmagic in C++17."
-
     settings = "os", "compiler", "arch", "build_type", "cppstd"
-    generators = "cmake_find_package"
+    options = {"shared": [True, False], "fPIC": [
+        True, False], "build_tests": [True, False], "benchmark": [True, False]}
+    default_options = "shared=False", "fPIC=True", "build_tests=False", "benchmark=True"
+    generators = "cmake_find_package", "cmake"
 
-    no_copy_source = True
+    def configure(self):
+        if self.settings.os == "Windows":
+            self.options.remove("fPIC")
 
     def source(self):
         git = tools.Git()
         git.clone("{}.git".format(self.url))
 
     def build(self):
-        cmake = CMake(self, generator="Ninja")
+        cmake = CMake(self)
+
         cmake.configure()
         cmake.build()
-        cmake.test()
+
+        if self.options.build_tests:
+            cmake.test()
 
     def package(self):
         self.copy("*.hpp", dst="include/blackmagic", src="include/blackmagic")
@@ -29,9 +37,7 @@ class Blackmagic(ConanFile):
         self.info.header_only()
 
     def requirements(self):
-        tag = "stable-{}-{}".format(self.settings.compiler, str(self.settings.build_type).lower())
+        self.requires("gtest/1.8.1@bincrafters/stable")
 
-        self.requires("gtest/1.8.0@dutiona/{}".format(tag))
-        # self.requires("gtest/1.8.0@mroynard/{}".format(tag))
-        self.requires("benchmark/1.4.1@dutiona/{}".format(tag))
-        # self.requires("benchmark/1.4.1@mroynard/{}".format(tag))
+        if self.options.benchmark:
+            self.requires("benchmark/1.4.1@dutiona/stable")
